8afb12214959405d12949e6e4cb95abb34a32161
JimmyGuo0223 <1259625032@qq.com>
2022-10-09 00:15:30 +0800

    add test button, has problems

--- app/src/main/java/com/example/todolist/LoginActivity.java
+++ app/src/main/java/com/example/todolist/LoginActivity.java
@@ -8,17 +8,31 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
 public class LoginActivity extends AppCompatActivity {
 
 
     Button loginBtn;
     Button registerBtn;
 
+    @BindView(R.id.btn_test)
+    Button test;
+
+    @OnClick(R.id.btn_test)
+    public void onClick(){
+        Intent intent =  new Intent(getApplicationContext(),TestActivity.class);
+        startActivity(intent);
+    }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        ButterKnife.bind(this);
 
 //        loginBtn = findViewById(R.id.btn_login);
 //        loginBtn.setOnClickListener(new View.OnClickListener() {
