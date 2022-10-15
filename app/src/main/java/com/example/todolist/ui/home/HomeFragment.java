02542fafb09583841b839b22235d4cd0a8cccab9
JimmyGuo0223 <1259625032@qq.com>
2022-10-15 23:55:20 +0800

    自动添加view

--- app/src/main/java/com/example/todolist/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/todolist/ui/home/HomeFragment.java
@@ -4,17 +4,27 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.example.todolist.R;
 import com.example.todolist.databinding.FragmentHomeBinding;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class HomeFragment extends Fragment {
-
     private FragmentHomeBinding binding;
+    private LinearLayout layout;
+
+    int i = 0;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -22,10 +32,23 @@ public class HomeFragment extends Fragment {
                 new ViewModelProvider(this).get(HomeViewModel.class);
 
         binding = FragmentHomeBinding.inflate(inflater, container, false);
+
+
         View root = binding.getRoot();
 
         final TextView textView = binding.textHome;
         homeViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+
+        layout = binding.eventLayout;
+
+        FloatingActionButton fab =  binding.fab;
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getContext(),"clicked fab", Toast.LENGTH_SHORT).show();
+                addView(root);
+            }
+        });
         return root;
     }
 
@@ -34,4 +57,16 @@ public class HomeFragment extends Fragment {
         super.onDestroyView();
         binding = null;
     }
+
+    public void addView(View view) {
+        TextView child = new TextView(getContext());
+        child.setTextSize(20);
+        // 获取当前的时间并转换为时间戳格式,
+        i++;
+        String currentTime = "add: " + i;
+        child.setText(currentTime);
+        // 调用一个参数的addView方法
+        layout.addView(child);
+    }
+
 }
