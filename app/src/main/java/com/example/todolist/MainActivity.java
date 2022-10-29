cb77030c64581eed70d0262c5318414c896cde22
Kexin Wen <leahkexinwen@gmail.com>
2022-10-30 01:20:23 +0800

    add light sensor to change background color based on luminosity

--- app/src/main/java/com/example/todolist/MainActivity.java
+++ app/src/main/java/com/example/todolist/MainActivity.java
@@ -1,6 +1,12 @@
 package com.example.todolist;
 
+import android.graphics.Color;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 
 import com.example.todolist.ui.timer.TimerViewModel;
@@ -12,13 +18,21 @@ import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
+
+
 import com.example.todolist.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+    public static final int TYPE_LIGHT = 5;
 
     private ActivityMainBinding binding;
-
     private String uid;
+
+    private SensorManager sensorManager;
+    private Sensor lightSensor;
+    private SensorEventListener lightEventListener;
+//    private View root;
+    private float maxValue;
 
     public String getUid() {
         return uid;
@@ -30,6 +44,24 @@ public class MainActivity extends AppCompatActivity {
 
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+
+        sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+        lightSensor = sensorManager.getDefaultSensor(TYPE_LIGHT);
+        maxValue = lightSensor.getMaximumRange();
+        lightEventListener = new SensorEventListener() {
+            @Override
+            public void onSensorChanged(SensorEvent sensorEvent) {
+                float value = sensorEvent.values[0];
+                int newValue = (int) (255f * value / maxValue);
+                binding.getRoot().setBackgroundColor(Color.rgb(newValue,newValue,newValue));
+            }
+
+            @Override
+            public void onAccuracyChanged(Sensor sensor, int i) {
+
+            }
+        };
+
 
         TextView tv = findViewById(R.id.login_username);
         Bundle bundle = this.getIntent().getExtras();
@@ -46,6 +78,15 @@ public class MainActivity extends AppCompatActivity {
         NavigationUI.setupWithNavController(binding.navView, navController);
     }
 
+    @Override
+    protected void onResume() {
+        super.onResume();
+        sensorManager.registerListener(lightEventListener,lightSensor,SensorManager.SENSOR_DELAY_FASTEST);
+    }
 
-
+    @Override
+    protected void onPause() {
+        super.onPause();
+        sensorManager.unregisterListener(lightEventListener);
+    }
 }
